97b903a381724097d126b4dfb9744eddc970af86
Venus <guanv2051@masseyhigh.school.nz>
2026-03-31 10:52:59 +1300

    Starting

---
+++ B_03_Quiz_Assessment_v1.py
@@ -0,0 +1,22 @@
+import csv
+import random
+from tkinter import *
+from functools import partial # To prevent unwanted windows
+
+class StartGame:
+    """
+    Initial Game Interface
+    """
+
+
+
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Quiz")
+    StartGame()
+    root.mainloop()
+
